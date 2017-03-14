e5ffe712d6950e3233a5d0ca5750f97386a91483
@rnarsina <rnarsina@gmail.com>
2017-03-14 09:17:53 -0700

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function will hold the global scope of cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function first will check the cache, 
+## if available, will return from the cache
+## if not available in cache, reverses the matrix and stores in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    message("Cached data did not found, will get the inverse and stored in cache")
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
